47f5ae30f085b8137e147ab952cad7683d45d1a6
m305-67\w401 <tmgfklrejhg@fcjofdmsl;.cko>
2018-05-10 18:20:33 +0800

    no message

---
+++ notebook1.0/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace notebook1._0
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+            if(result==true)
+            {
+                System.IO.File.WriteAllText(dlg.FileName, AAA.Text);
+
+                 }
+            System.IO.File.WriteAllText(@"C:\temp\aaa.txt", AAA.Text);
+
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = dlg.ShowDialog();
+            if(result==true)
+            {
+                AAA.Text = System.IO.File.ReadAllText(dlg.FileName);
+            }
+        }
+    }
+}
